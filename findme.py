1d9f419f75ac12a5c40fe7ca208d9c5bceffaf3a
Richard Bronkhorst <richardbronkhorst@hotmail.com>
2017-01-15 13:57:20 +0100

    first commit

---
+++ findme.py
@@ -0,0 +1,13 @@
+import socket
+myHostname = socket.gethostname()
+UDP_IP = "0.0.0.0"
+UDP_PORT = 55585
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+  data, addr = sock.recvfrom(1024)
+  if data == "Hello?":
+    print("Got someone searching")
+    sock.sendto("Hello, i am " + myHostname, addr)
---
+++ search.py
@@ -0,0 +1,18 @@
+import socket
+myHostname = socket.gethostname()
+UDP_IP = "0.0.0.0"
+UDP_PORT = 55586
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+print("Sending broadcast: Hello?")
+sock.sendto("Hello?", ("255.255.255.255", UDP_PORT - 1))
+sock.settimeout(10)
+while True:
+  try:
+    data, addr = sock.recvfrom(1024)
+  except:
+    break
+  print("Got response from %s: %s " % (addr[0], data))
+  
