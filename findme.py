5d9559345e8d5bb8004c31d2fa09f4cdfa2ad869
Richard Bronkhorst <richardbronkhorst@hotmail.com>
2017-09-08 14:44:28 +0200

    quit when port is already bound

--- findme.py
+++ findme.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 import socket
+import sys
 myHostname = socket.gethostname()
 UDP_IP = "0.0.0.0"
 UDP_PORT = 55585
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+try:
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.bind((UDP_IP, UDP_PORT))
+except socket.error:
+   sys.exit(0)
 
 while True:
   data, addr = sock.recvfrom(1024)
